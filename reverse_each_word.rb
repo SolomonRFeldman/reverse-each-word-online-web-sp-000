f9cd4fddeab405decbaed66b0c4b46011f8645ba
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 20:08:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(sentence)
   reversed = []
-  sentence.split(" ").each { |word| word.reverse << reversed }
+  sentence.split(" ") = new_array
+  .each { |word| word.reverse << reversed }
   reversed
 end
+
 sentence = "Hi my name is Paul. Nice to meet you."
 print reverse_each_word(sentence)
