154ee582fec39c2f4d44951e69fc1b8d39c02160
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 20:06:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,5 @@ def reverse_each_word(sentence)
   sentence.split(" ").each { |word| word.reverse << reversed }
   reversed
 end
+sentence = "Hi my name is Paul. Nice to meet you."
+print reverse_each_word(sentence)
